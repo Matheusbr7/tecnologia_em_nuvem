8ce9111e4419229c5d6e4ab75ee776c5dacb60c5
Matheusbr7 <matheus.roberto01@etec.sp.gov.br>
2025-09-22 09:22:34 -0300

    Create converter_binario.php

---
+++ app3/converter_binario.php
@@ -0,0 +1,21 @@
+<?php
+//Função para converter uma string para binario
+function stringParaBinario($string) {
+//A função str_split divide a string em um array de caracteres
+//A função ord pega o valor ASCII de cada caractere
+//A função decbin converte o valor ASCII em binario
+$binario = '';
+  foreach(str_split($string) as $char) {
+    //Converter o caractere para o valor binario de 8 bits 
+  $binario .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . ' ';
+  }
+  return $binario;
+  //Exemplo de uso
+$frase = "Matheus Bastos Roberto";
+  $fraseBinario = strinParaBinario($frase);
+
+//Exibindo a frase original e sua versão binaria 
+echo "Frase original: " . $frase . "<br>";
+echo "Frase em binário " . $fraseBinario;
+
+?>
